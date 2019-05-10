335ff85fbb1203cce5a26327c97745160ec9b5a3
Jesper Hooijmans <36195503+w1-hooijmansjesper@users.noreply.github.com>
2019-05-10 12:54:26 +0200

    Add files via upload

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,46 @@
+<template>
+  <header class="header">
+    <div class="Ikea">
+    <h1>IKEA<span style="font-size: 13px; vertical-align: text-top;">®</span> Boodschappen Lijstje</h1>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">Over ons</router-link>
+    </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped>
+  .header {
+    background: #004FB5;
+    color: #F8D500;
+    text-align: center;
+    padding: 30px;
+  }
+
+  .header a {
+    color: #004FB5;
+    background: #F8D500;
+    border-radius: 30px;
+    padding: 10px;
+    text-decoration: none;
+  }
+  .Ikea
+  {
+    color: #004FB5;
+    background: #F8D500;
+    width: 100%;
+    height: 100px;
+    border-radius: 60%;
+  }
+  
+
+</style>
+
+
---
+++ src/views/Home.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="app">
+    <AddTodo v-on:add-todo="addTodo" />
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+  </div>
+</template>
+
+<script>
+import Todos from '../components/Todos';
+import AddTodo from '../components/AddTodo';
+import axios from 'axios';
+
+export default {
+  name: 'Home',
+  components: {
+    Todos,
+    AddTodo
+  },
+  data() {
+    return {
+      todos: []
+    }
+  },
+  methods: {
+    deleteTodo(id) {
+      axios.delete(`https://my-json-server.typicode.com/w1-schmitzniels/FrontendVue/posts/${id}`)
+        .then(res => this.todos = this.todos.filter(todo => todo.id !== id))
+        .catch(err => console.log(err));
+    },
+    addTodo(newTodo) {
+      const { title, completed } = newTodo;
+
+      axios.post('https://my-json-server.typicode.com/w1-schmitzniels/FrontendVue/posts', {
+        title,
+        completed
+      })
+        .then(res => this.todos = [...this.todos, res.data])
+        .catch(err => console.log(err));
+    }
+  },
+  created() {
+    axios.get('https://my-json-server.typicode.com/w1-schmitzniels/FrontendVue/posts')
+      .then(res => this.todos = res.data)
+      .catch(err => console.log(err));
+  }
+}
+</script>
+
+<style>
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
+
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+
+  .btn:hover {
+    background:blue;
+  }
+</style>
