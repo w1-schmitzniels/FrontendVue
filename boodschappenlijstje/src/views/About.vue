b81828dc9d86729d85dd15f063b68d2503798a21
JesperHooijmans <jesper.hooijmans@edu-kw1c.nl>
2019-05-24 12:00:44 +0200

    Revert "burger menu"
    This reverts commit 6cafff51bfdf12b85dbf827748af819baa9096f9.

--- boodschappenlijstje/src/views/About.vue
+++ boodschappenlijstje/src/views/About.vue
@@ -1,22 +1,6 @@
 <template>
   <div class="about">
-    <h1>Ikea boodschappen lijstje </h1>
+    <h1>About</h1>
     <p>Dit is een boodschappen lijst</p>
   </div>
 </template>
-<style>
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-    background-attachment: fixed;
-
-  }
-
-  body {
-    font-family: Arial, Helvetica, sans-serif;
-    line-height: 1.4;
-  }
-
-  
-</style>
